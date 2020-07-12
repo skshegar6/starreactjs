a1059aefec62ea1444c22d1ada1cc141f363d105
skshegar6 <68197765+skshegar6@users.noreply.github.com>
2020-07-12 23:27:40 +0530

    Update planetsApi.js

--- src/js/redux/api/planetsApi.js
+++ src/js/redux/api/planetsApi.js
@@ -2,13 +2,13 @@ import { handleResponse,handleError} from './apiUtils';
 const API_BASE_ADDRESS = 'https://swapi.dev/api/planets';
 
 export function getPlanets(){
-  return fetch(API_BASE_ADDRESS,{mode: "no-cors"})
+  return fetch(API_BASE_ADDRESS)
     .then(handleResponse)
     .catch(handleError);
 }
 
 export function getRemaningPlanets(page_no){
-  return fetch(API_BASE_ADDRESS+'?page='+page_no,{mode: "no-cors"})
+  return fetch(API_BASE_ADDRESS+'?page='+page_no)
     .then(handleResponse)
     .catch(handleError);
 }
