569089a4d5952174f340752e66fb67f26d47654c
skshegar6 <68197765+skshegar6@users.noreply.github.com>
2020-07-12 23:25:59 +0530

    Update loginUsersApiActions.js

--- src/js/redux/api/loginUsersApiActions.js
+++ src/js/redux/api/loginUsersApiActions.js
@@ -2,7 +2,7 @@ import { handleResponse,handleError} from './apiUtils';
 const API_BASE_ADDRESS = 'https://swapi.dev/api/';
 
 export function checkLoginUser(value){
-  return fetch(API_BASE_ADDRESS+"people/?search="+value,{mode: "no-cors"})
+  return fetch(API_BASE_ADDRESS+"people/?search="+value)
     .then(handleResponse)
     .catch(handleError);
 }
